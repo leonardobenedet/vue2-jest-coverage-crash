3ab3970dc860537df981023105e3cdc3309ad5fe
leonardobenedet <leonardo.benedet@contaazul.com>
2022-09-15 17:31:24 -0300

    4 files modified

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,25 @@
+const path = require('path');
+
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
-}
+  rootDir: path.resolve(__dirname, '../../'),
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '.*\\.(vue)$': '<rootDir>/node_modules/@vue/vue2-jest',
+    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost',
+  },
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  setupFiles: ['<rootDir>/test/unit/setup'],
+  coverageDirectory: '<rootDir>/test/unit/coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!**/node_modules/**',
+  ],
+};
